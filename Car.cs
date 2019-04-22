bbc48adae2b5d8ca9f45c19e787668b419cbb277
Dustin Herboldshimer and Marina Poltorak <dustin@>
2019-04-22 09:55:56 -0700

    add WorthBuying() to class Car and refactor in Main()

--- Car.cs
+++ Car.cs
@@ -6,6 +6,11 @@ class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+
+  public bool WorthBuying(int maxPrice)
+  {
+    return (Price < maxPrice);
+  }
 }
 
 public class Program
@@ -36,6 +41,25 @@ public class Program
     List<Car> Cars = new List<Car>() { porsche, ford, lexus, mercedes };
 
     foreach(Car automobile in Cars)
+    {
+      Console.WriteLine(automobile.MakeModel);
+    }
+
+    Console.WriteLine("Enter maximum price: ");
+    string stringMaxPrice = Console.ReadLine();
+    int maxPrice = int.Parse(stringMaxPrice);
+
+    List<Car> CarsMatchingSearch = new List<Car>();
+
+    foreach (Car automobile in Cars)
+    {
+      if (automobile.WorthBuying(maxPrice))
+      {
+        CarsMatchingSearch.Add(automobile);
+      }
+    }
+
+    foreach(Car automobile in CarsMatchingSearch)
     {
       Console.WriteLine(automobile.MakeModel);
     }
